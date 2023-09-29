dc10d25c4aaea7f4262193ee05b92c2d3237764a
MaxGroshev <groshevmaxim84@yandex.ru>
2023-09-30 00:48:41 +0300

    added big data tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -3,8 +3,9 @@
 //-----------------------------------------------------------------------------------------
 
 int main () {
-    test_perf_lru ();
-    test_two_q    ();
+    // test_perf_lru ();
+    // test_two_q    ();
+    comp_perf_and_2q ();
     return 0;
 }
 
@@ -74,12 +75,83 @@ int test_two_q () { //repeating of the previos code, but impossible to change in
     return 0;
 }
 
+int comp_perf_and_2q () {
+
+    using namespace time_control;
+
+    std::ifstream input_file;
+    input_file.open ("./tests/test_data/comp_test.txt");
+	ASSERT (input_file.is_open ());
+
+    int    perf_lru_hits = 0;
+    int    two_q_hits    = 0;
+    size_t cache_size    = 0;
+    size_t count_of_elem = 0;
+    int    perf_lru_correct_res = 0;
+    int    two_q_correct_res    = 0;
+
+    chrono_time_type start_time;
+    chrono_time_type end_time;
+
+    std::cout << "-----------------COMPARISON_TESTS--------------------\n";
+    for (int num_of_test = 1; num_of_test <= count_of_comp_test; num_of_test++) {
+        get_init_data (&cache_size, &count_of_elem, input_file);
+
+        cache::perf_lru_t<int> perf_lru {cache_size};
+        cache::two_q_cache_t<int> lru_2q {cache_size};
+        auto user_data = perf_lru.get_user_data (count_of_elem, input_file);
+
+        start_time = chrono_cur_time ();
+        perf_lru_correct_res = get_correct_res (input_file);
+        perf_lru_hits = perf_lru.test_data (user_data);
+        end_time = chrono_cur_time ();
+        double perf_lru_time = (end_time - start_time) / 0.001ms;
+
+        start_time = chrono_cur_time ();
+        two_q_correct_res = get_correct_res (input_file);
+        two_q_hits = lru_2q.test_data (user_data);
+        end_time = chrono_cur_time ();
+        double two_q_time = (end_time - start_time) / 0.001ms;
+
+        show_comp_res (num_of_test, perf_lru_hits, two_q_hits,
+                       perf_lru_correct_res, two_q_correct_res,
+                       perf_lru_time, two_q_time);
+    }
+
+    input_file.close ();
+    return 0;
+}
+
+
 //-----------------------------------------------------------------------------------------
 
 int get_correct_res (std::istream & in_strm) {
     int correct_res = 0;
     in_strm >> correct_res;
     return correct_res;
+}
+
+int show_comp_res (int num_of_test, int perf_lru_hits, int two_q_hits,
+                   int perf_lru_correct_res, int two_q_correct_res,
+                   double perf_lru_time, double two_q_time) {
+
+    using namespace time_control;
+
+    if (perf_lru_hits == perf_lru_correct_res && two_q_hits == two_q_correct_res) {
+        printf ("|%s Test %d IS PASSED:%s\n", GREEN_C, num_of_test, RESET_C);
+    }
+    else {
+        printf ("|%s Test %d IS FAILED:%s\n", RED_C, num_of_test, RESET_C);
+        printf ("%s [perf_lru_hits: %d; perf_lru_correct_res: %d]%s\n",
+        BLUE_C, perf_lru_hits, perf_lru_correct_res, RESET_C);
+        printf ("%s [two_q_hits: %d; two_q_correct_res: %d]%s\n",
+        BLUE_C, two_q_hits, two_q_correct_res, RESET_C);
+
+    }
+    printf ("|Perf lru running tine: %lg microseconds\n", perf_lru_time);
+    printf ("|2q running time:       %lg microseconds\n", two_q_time);
+
+    return 0;
 }
 
 int show_test_res (int num_of_test, int hits, int correct_res, double test_time) {
--- tests/testing.hpp
+++ tests/testing.hpp
@@ -18,13 +18,18 @@
 #include "../debug_utils/console_colors.hpp"
 
 const int count_of_perf_lru_tests = 7;
-const int count_of_2_q_tests      = 5;
+const int count_of_2_q_tests      = 6;
+const int count_of_comp_test      = 11;
 
 //-----------------------------------------------------------------------------------------
 
 int test_perf_lru   ();
 int test_two_q      ();
+int comp_perf_and_2q();
 int get_correct_res (std::istream & in_strm);
 int show_test_res   (int num_of_test, int hits, int correct_res, double test_time);
+int show_comp_res (int num_of_test, int perf_lru_hits, int two_q_hits,
+                   int perf_lru_correct_res, int two_q_correct_res,
+                   double perf_lru_time, double two_q_time);
 
 #endif
